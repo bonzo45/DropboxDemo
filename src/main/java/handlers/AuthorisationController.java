fa949d5274245e26e976472c1e1c4d0c302bec2e
Sam Esgate <bonzo450@hotmail.com>
2014-04-29 14:56:24 +0100

    Dropbox now authenticates and redirects with a code.
    I now need to get this code to be displayed on the main page. Currently
    it returns it on it’s own.

--- src/main/java/handlers/AuthorisationController.java
+++ src/main/java/handlers/AuthorisationController.java
@@ -2,24 +2,19 @@ package handlers;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-
-import org.apache.commons.codec.binary.Base64;
 
 import dropbox.RESTDropbox;
 
@@ -32,7 +27,21 @@ import dropbox.RESTDropbox;
 public class AuthorisationController {
 
   /**
+   * A way of storing Dropbox (DbxWebAuth) objects to persist state between the
+   * two calls. Usage: DbxWebAuth object is added when the user is re-directed
+   * to dropbox. DbxWebAuth object is removed when they are returned to the
+   * site.
+   * 
+   * Known Issues: If the user is never returned the object is never deleted ->
+   * MEMORY LEAK Ignored in this demo as each following state overwrites the
+   * last.
+   */
+  private static ConcurrentHashMap<Integer, RESTDropbox> dbxStates = new ConcurrentHashMap<>();
+
+  /**
    * Return JSON containing this application's authorisation link.
+   * 
+   * NOTE: If using in a Web App, use redirectAuthorisation instead.
    * 
    * @return
    */
@@ -45,13 +54,15 @@ public class AuthorisationController {
     return "{\"auth_link\": \"" + auth_link + "\"}";
   }
 
-  // Generate a random string to use as a CSRF token.
-  private static String generateCSRFToken() {
-    byte[] b = new byte[18];
-    new SecureRandom().nextBytes(b);
-    return Base64.encodeBase64URLSafeString(b);
-  }
-
+  /**
+   * Redirects the user to the Dropbox authentication page. This page then
+   * returns the user to the application.
+   * 
+   * @param request
+   *          - Servlet request (used by Dropbox for some strange reason?)
+   * @return
+   * @throws URISyntaxException
+   */
   @GET
   @Path("redirect_to_dropbox")
   public Response redirectAuthorisation(@Context HttpServletRequest request)
@@ -60,38 +71,44 @@ public class AuthorisationController {
 
     // Get Session
     HttpSession session = request.getSession(true);
-    // Generate new Session Key
+    // Session Key's Name
     String sessionKey = "dropbox-auth-csrf-token";
     // Return here when Dropbox has finished authenticating.
-    String returnURI = "http://localhost:8080/DropboxDemo/dropbox/auth/back_from_dropbox";
+    String returnURI = "http://localhost:8080/DropboxDemo/dropbox/auth/redirect_from_dropbox";
 
-    String dropboxRedirect = dropbox.getAuthorisationRedirect(session, sessionKey, returnURI);
-    // Re-direct user to dropbox, setting the session key as a cookie so we can
-    // check it later.
-    return Response.seeOther(new URI(dropboxRedirect))
-        .cookie(new NewCookie("csrf", sessionKey)).build();
+    // Get redirect URL
+    String dropboxRedirect = dropbox.getAuthorisationRedirect(session,
+        sessionKey, returnURI);
+    // Store the Dropbox State so we can do a CSRF check when they return.
+    dbxStates.put(1, dropbox);
+    // Re-direct user to dropbox.
+    return Response.seeOther(new URI(dropboxRedirect)).build();  
   }
 
+  /**
+   * When the user returns from Dropbox Authentication they are sent here.
+   * 
+   * @param token
+   *          - used for CSRF detection
+   * @param authenticationCode
+   *          - Authentication Code used to get Access Token
+   * @return
+   */
   @GET
-  @Path("back_from_dropbox")
-  public Response backFromDropbox(@Context HttpServletRequest request,
-      @CookieParam(value = "csrf") String sessionKey,
-      @QueryParam("state") String dropboxSessionKey,
+  @Path("redirect_from_dropbox")
+  public String backFromDropbox(@QueryParam("state") String token,
       @QueryParam("code") String authenticationCode) {
-    RESTDropbox dropbox = new RESTDropbox();
+    // Retrieve the Dropbox 'State' from storage.
+    RESTDropbox dropbox = dbxStates.remove(1);
 
-    // Get Session
-    HttpSession session = request.getSession(true);
- 
-    // Check Session Key matches
-    if (sessionKey.equals(dropboxSessionKey)) {
-      
-    }
-    
-    // TODO: http://dropbox.github.io/dropbox-sdk-java/api-docs/v1.7.x/com/dropbox/core/DbxWebAuth.html
+    // Reconstruct the parameter map - Jersey consumed it :-(
+    HashMap<String, String[]> hashMap = new HashMap<String, String[]>();
+    String[] state = { token };
+    String[] code = { authenticationCode };
+    hashMap.put("state", state);
+    hashMap.put("code", code);
 
-    
-    return null;
+    return dropbox.getAccessTokenRedirect(hashMap);
   }
 
   /**
