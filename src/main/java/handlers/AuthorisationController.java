ac66fb90eec280eac9799d306cd254abaec1ac40
Sam Esgate <bonzo450@hotmail.com>
2014-04-30 11:48:27 +0100

    Added exceptions to backFromDropbox. Now it 404s.

--- src/main/java/handlers/AuthorisationController.java
+++ src/main/java/handlers/AuthorisationController.java
@@ -2,7 +2,6 @@ package handlers;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,12 +10,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import org.glassfish.jersey.server.mvc.Viewable;
 
 import dropbox.RESTDropbox;
 
