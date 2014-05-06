fae4741f252cc8ee1ca018d596d7064cc85b9a08
Sam Esgate <bonzo450@hotmail.com>
2014-05-06 17:26:46 +0100

    Eclipse needed cleaning for some reason.

--- src/main/java/storage/cloud/dropbox/DropboxFile.java
+++ src/main/java/storage/cloud/dropbox/DropboxFile.java
@@ -1,9 +1,15 @@
 package storage.cloud.dropbox;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import model.FileMetadata;
+import storage.SamFile;
 import util.FileUtil;
 
-public class DropboxFile {
-  
+public class DropboxFile extends SamFile {
+
   public DropboxFile(String relativeFilePath) {
     String fileName = FileUtil.extractName(relativeFilePath);
     String filePath = "/" + FileUtil.extractPath(relativeFilePath);
@@ -12,16 +18,20 @@ public class DropboxFile {
   
   @Override
   public InputStream getInputStream() throws FileNotFoundException {
-    throw new UnsupportedOperationException();
+    // TODO Auto-generated method stub
+    return null;
   }
 
   @Override
   public OutputStream getOutputStream() throws FileNotFoundException {
-    throw new UnsupportedOperationException();
+    // TODO Auto-generated method stub
+    return null;
   }
 
   @Override
   public void addCopy(String place, String path) {
-    throw new UnsupportedOperationException();
+    // TODO Auto-generated method stub
+    
   }
+
 }
